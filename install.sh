d1b7265bfed0acbfb83486a97253c6dfcb4a3424
emilio <emilio@gopangea.com>
2017-03-14 19:51:06 +0100

    install

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install numpy
+pip install numpy
+pip install theano
+pip install lasagne
+pip install --upgrade https://github.com/Theano/Theano/archive/master.zip
+pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
+
+python test.py
